30a3e97d50255f4c55fa5f1d91a70869baca0a23
obelisk0114 <obelisk0114@yahoo.com.tw>
2018-03-20 02:48:17 -0700

    Change to my data
    Change to my data

--- src/project2/MainRunner.java
+++ src/project2/MainRunner.java
@@ -4,13 +4,28 @@ import java.io.*;
 import java.util.*;
 
 public class MainRunner {
-	static final String FILE_NAME = "cs_205_large33.txt";
+	// CS205_SMALLtestdata__41.txt
+	static String FILE_NAME = "205_proj2_data/CS205_BIGtestdata__20.txt";
 	
 	public static void main(String[] args){
 
 		Tester mainTester = new Tester();
 
 		try {
+			switch (args.length) {
+			case 0:
+				break;
+			case 1:
+				FILE_NAME = args[0];
+				break;
+			default:
+				System.out.println("\n    Invalid input parameters!"
+						+ "\n    Usage: java -jar nn.jar         "
+						+ "# FILE_NAME = 205_proj2_data/CS205_BIGtestdata__20.txt"
+						+ "\n    Usage: java -jar nn.jar <s>     # data = s\n");
+				System.exit(1);
+			}
+			
 			Scanner sc = new Scanner(new File(FILE_NAME));
 			while(sc.hasNextLine()){
 				Scanner lineSc = new Scanner(sc.nextLine());
